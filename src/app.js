1913b5c4dca03a8c3b069aef4a5b631e63c011f9
Víctor Roberto León Guerra <victorleon.dev@gmail.com>
2025-03-14 20:18:49 +0000

    hecho

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-import "bootstrap";
-import "./style.css";
+const prefijos = ["super", "ultra", "mega", "thelegendof"];
+const juegos = ["zelda", "mario", "assassinscreed", "minecraft", "skyrim"];
+const sufijos = ["world", "series", "chronicles", "odyssey", "planet"];
+const extensiones = [".es", ".com", ".net", ".org"];
 
+const generarDominios = (pref, juegos, suf, exts) =>
+  exts.reduce((acc, ext) => (
+    acc[ext] = pref.flatMap(p =>
+      juegos.flatMap(j =>
+         suf.map(s => {
+           const extLetras = ext.slice(1).toLowerCase();
+           const modS = s.toLowerCase().endsWith(extLetras) ? s.slice(0, -extLetras.length) : s;
+           return `${p}${j}${modS}${ext}`;
+         })
+      )
+    ).sort(), acc
+  ), {});
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const dominios = generarDominios(prefijos, juegos, sufijos, extensiones);
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+(() => {
+  const res = document.getElementById('resultado');
+  Object.keys(dominios).sort().forEach(ext => {
+    const div = document.createElement('div');
+    div.className = 'extension';
+    div.innerHTML = `<h2>${ext}</h2><ul>${dominios[ext].map(d => `<li>${d}</li>`).join('')}</ul>`;
+    res.appendChild(div);
+  });
+})();
